06486a79eb7069f25c7966a919f01f240e168f22
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-06-23 13:19:07 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,24 @@
+from Bio.Seq import Seq
+
+k1 = Seq('CATAAGA')
+k2 = Seq('AAACAGA')
+
+# Calculate the Hamming distance
+hamming_distance = sum(a != b for a, b in zip(k1, k2))
+
+print(hamming_distance)
+
+
+def hamming_distance(string1, string2):
+    distance = 0
+    L = len(string1)
+    for i in range(L):
+        if string1[i] != string2[i]:
+            distance += 1
+    return distance
+
+string1 = input("Please enter the first sequence: ")
+string2 = input("Please enter the second sequence: ")
+
+distance = hamming_distance(string1, string2)
+print("Hamming distance:", distance)
